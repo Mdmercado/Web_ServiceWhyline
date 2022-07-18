e97f849a91bd19ede5f0e0c443a651c5cdccc2c1
Mdmercado <marcosmerk156@gmail.com>
2022-07-18 10:50:55 -0300

    feat: integrating new configurations

--- whyline/buscador/app.js
+++ whyline/buscador/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const datos_whyline = require("./datos_whyline");
 const datos_whyline_subscription = require("./datos_whyline_subscription");
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "40000MB" }));
+app.use(bodyParser.json({ limit: "40000MB" }));
 
 // index routes
 app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
-//Ruta coches usa a la funcion consulta
+//Rutas API buscador
 app.get("/whyline/:desde/:hasta", datos_whyline.consulta);
 app.get("/subscriptions/:desde/:hasta", datos_whyline_subscription.consulta);
 
 app.listen(8080, () => {
-  console.log("server iniciado en puerto 8080");
+  console.log("server iniciado");
 });
